b9af0bbe34235f5ba0bdb823d01a8113a5d45cf9
IZEKKI Jalal <Jalal.Izekki@bordeaux-inp.fr>
2021-04-17 03:53:10 +0200

    restructuration du code de l'affichage, client liée a l'interface

--- affichage/src/aquarium/Aquarium.java
+++ affichage/src/aquarium/Aquarium.java
@@ -1,78 +1,31 @@
 package aquarium;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
+import aquarium.gui.Fish;
+import aquarium.gui.Viewer;
+import javafx.application.Platform;
+
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Aquarium {
-    private final ArrayList<Fish> fish = new ArrayList<Fish>();
-    private final ArrayList<Viewer> viewers = new ArrayList<Viewer>();
-    private final String name = "Aquarium";
-    private String backgroundColor = "black";
+    public final ArrayList<Fish> fish = new ArrayList<Fish>();
+    public final ArrayList<Connection> connections = new ArrayList<Connection>();
     int width;
     int height;
 
-    public void addViewer(Viewer v){
-        this.viewers.add(v);
-    }
-
-    public void addFish(Fish f){
-        fish.add(f);
-    }
-
-    public void delFish(String fishName){
-        this.fish.removeIf(f -> f.getName().equals(fishName));
-    }
-
-    public void delViewer(String viewerName){
-        viewers.removeIf(v -> v.getName().equals(viewerName));
-    }
-
-    public void create() throws IOException{
-        parseFile("resources/data/aquarium.txt");
-        for (Viewer v : viewers) {
-            Pane pane = test();
-            Scene scene = new Scene(pane, v.viewer_width, v.viewer_height);
-            pane.setStyle("-fx-background-color: linear-gradient(to left, #1fdedb, #1f7c91);");
-
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.setTitle(v.getName());
-            stage.setResizable(false);
-            stage.show();
-
-            break; // TO BE REMOVED !!!!!!!!!!!
-
+    public Aquarium(String filename){
+        parseFile(filename);
+        for (Connection c : connections) {
+            Thread thread = new Thread(() -> {
+                Platform.runLater(() -> c.viewer.stage.show());
+                c.launch();
+            });
+            thread.setDaemon(true);
+            thread.start();
         }
     }
 
-    public void moveFish(){
-        for (Fish f : fish){
-            f.move();
-        }
-    }
-
-    Pane test(){
-        Pane pane = new Pane();
-        Fish f;
-        Random r = new Random();
-        for (int i = 0; i < 40; i++) {
-            int w = r.nextInt(viewers.get(0).getWidth())/10;
-            if (w>100){
-                w /=10;
-            }
-            f = new Fish(r.nextInt(viewers.get(0).getWidth()), r.nextInt(viewers.get(0).getWidth()), w, w);
-            fish.add(f);
-            pane.getChildren().add(f.getImageView());
-        }
-        pane.setStyle(String.format("-fx-background-color: %s;", this.backgroundColor));
-        return pane;
-    }
-
-    void parseFile(String filename) throws IOException {
+    void parseFile(String filename) {
         try {
             FileInputStream fs = new FileInputStream(filename);
             DataInputStream in = new DataInputStream(fs);
@@ -82,22 +35,26 @@ public class Aquarium {
             String line = scanner.nextLine();
             this.width = Integer.parseInt(line.split("x")[0]);
             this.height = Integer.parseInt(line.split("x")[1]);
-            Viewer v;
 
             while (scanner.hasNextLine()) {
                 line = scanner.nextLine();
-                String name = new String(line.split(" ")[0]);
+                String name = line.split(" ")[0];
                 int viewer_x = Integer.parseInt(line.split(" ")[1].split("\\+")[0].split("x")[0]);
                 int viewer_y = Integer.parseInt(line.split(" ")[1].split("\\+")[0].split("x")[1]);
                 int viewer_width = Integer.parseInt(line.split(" ")[1].split("\\+")[1]);
                 int viewer_height = Integer.parseInt(line.split(" ")[1].split("\\+")[2]);
-                v = new Viewer(name, viewer_x, viewer_y, viewer_width, viewer_height);
-                this.addViewer(v);
+                Connection c = new Connection(name);
+                c.viewer = new Viewer(name, viewer_x, viewer_y, viewer_width, viewer_height);
+                this.connections.add(c);
             }
         }
         catch (IOException e){
             System.out.println("Error, couldn't open file");
         }
     }
+
+    public void addConnection(Connection c) { this.connections.add(c);}
+    public void delConnection(String name){ this.connections.removeIf(connection -> connection.getName().equals(name));}
+    public void addFish(Fish fish) { this.fish.add(fish);}
 
 }
--- affichage/src/aquarium/gui/Fish.java
+++ affichage/src/aquarium/gui/Fish.java
@@ -1,4 +1,4 @@
-package aquarium;
+package aquarium.gui;
 
 import javafx.animation.*;
 import javafx.scene.image.Image;
@@ -15,14 +15,15 @@ public class Fish {
     private PathTransition pathTransition;
     String name;
 
-    Fish(int x, int y, int fish_width, int fish_height){
-        iv = new ImageView();
-        iv.setImage(new Image("file:resources/images/clown-fish.png"));
+    public Fish(String name, int x, int y, int fish_width, int fish_height){
+        this.name = name;
+        this.iv = new ImageView();
+        this.iv.setImage(new Image("file:resources/images/clown-fish.png"));
 
-        iv.setFitWidth(fish_width);
-        iv.setFitHeight(fish_height);
-        iv.setX(x);
-        iv.setY(y);
+        this.iv.setFitWidth(fish_width);
+        this.iv.setFitHeight(fish_height);
+        this.iv.setX(x);
+        this.iv.setY(y);
 
         Path path = new Path();
         Random r = new Random();
@@ -38,19 +39,17 @@ public class Fish {
         this.pathTransition = new PathTransition();
         pathTransition.setDuration(Duration.millis(30000));
         pathTransition.setPath(path);
-        pathTransition.setNode(iv);
+        pathTransition.setNode(this.iv);
         pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-        //pathTransition.setCycleCount(Timeline.INDEFINITE);
-        //pathTransition.setAutoReverse(true);
     }
 
     public ImageView getImageView() {
-        return iv;
+        return this.iv;
     }
     public PathTransition getPathTransition() {
         return this.pathTransition;
     }
-    public String getName(){return name;}
+    public String getName(){return this.name;}
 
     public void move(){
         this.pathTransition.play();
